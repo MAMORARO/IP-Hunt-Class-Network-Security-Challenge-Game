1a1b857d24042bf7ad66caebf9c8166c9c975702
Tshepho Mamoraro Maimela <4323138@myuwc.ac.za>
2026-03-09 06:42:46 +0200

    I changed the entire code.

--- main.py
+++ main.py
@@ -1,67 +1,215 @@
-import socket 
+import mysql.connector 
+from datetime import datetime
+import hashlib
 import getpass
 import sys
 
+
 class GameClient:
 
+    def __init__(self): # constructor
+        self.user_name = None
+        self.password = None
+        self.connection = None
+        self.cursor = None
+
+    def get_user_input(self):
+        self.user_name = input("Enter your username :")
+        self.password = getpass.getpass("Enter your password :")
+
+    def connect_database(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host = "localhost",
+                port = 3306,
+                user = self.user_name,
+                password = self.password,
+                database = "ip_hunt_class"
+            )
+            self.cursor = self.connection.cursor()
+            print ("connected successfully")
+
+        except mysql.connector.Error:
+            print ("connection failed")
+            sys.exit()
+     
+    def check_progress(self):
         
-    server_ip_address=(input(" Enter IP Address  e.g  :"))
-    port_number =int(input("Enter Port Number e.g :"))
-    user_name=str(input("Enter Your Username e.g :"))
-    password=getpass.getpass("Enter Your password e.g  FirstInitial (UPPERCASE) + surname (lowercase) + _2026 :")
-    
+        try:
+            
+            self.select_query =self.cursor.execute("select current_level from hunt_progress where username=%s",(self.user_name,))
 
-    def __init__(self,server_ip_address,port_number,user_name,password):
-        self.server_ip_address =server_ip_address
-        self.port_number=port_number
-        self.user_name =user_name
-        self.password =password
+            results = self.cursor.fetchone()
 
-    def connect_server(self):
+            if results is None:
+                self.cursor.execute("insert into hunt_progress(username,current_level,completed) values(%s, 1, 0)",(self.user_name,))
+                self.connection.commit()
+                self.current_level =1
+                print("New player! starting at level 1.")
+            else:
+                self.current_level = results[0]
+                print(f"Welcome back! continuing from level {self.current_level}.")   
+
+        except mysql.connector.Error:
+              print("error checking progress has occurred.")
+              sys.exit() 
+
+    def get_challenge(self):
         
-           self.socket= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-           try:
-                self.socket.connect((self.server_ip_address,self.port_number))
-                print("Server connected successfully!.")
+        try:
+            
+            self.cursor.execute("select suspicious_ip and hint from challenges where username=%s and level=%s",(self.user_name, self.current_level))
+            results =self.cursor.fetchone()
 
-           except ConnectionRefusedError:
-                print(" Server failed to connect!.")
-                sys.exit()
+            if results is None:
+               print("No challenge found for this level.")
+               sys.exit()
+            else:
+                print("="*40)
+                print(f"Level {self.current_level}")
+                print("="*40)
+                print(f" Hint: {self.hint}")
+                print("="* 40)
 
-           except TimeoutError:
-                print("Server Time out error!.")
-                sys.exit()
+        except  mysql.connector.Error:
+            print("Error retrieving challenge.")
+            sys.exit()              
 
-           except Exception:
-                print("Unexpected error has occurred!.")     
-                sys.exit()
+    def  verify_answer(self):
+        
+        try:
+            
+            while True:
+                user_answer = input("Enter your flag answer:")
 
-    def Login(self):
-          
-         self.socket.send(self.user_name.encode())
-         self.socket.send(self.password.encode())
-
-         self.response = self.socket.recv(1024).decode() # decode response from the server into string data type.
-
-         try:
-             if "successful" in self.response or "welcome" in self.response:
-                   print("Login was sucessful")
-             else:
-                   print("login  was unsuccessful.")
-                   self.socket.close()
-                   sys.exit()
-         except ConnectionResetError:
-                    print("server has disconnected.")
-                    self.socket.close()
-                    sys.exit()
-
-              
-             
-
-         
+                combined = user_answer + self.user_name
+                hash_input =hashlib.sha256(combined.encode()).hexdigest()
 
 
+                self.cursor.execute("select flag_hash from challenges where username =%s and level =%s", (self.user_name,self.current_level))
 
+                results = self.cursor.fetchone()
+                flag_hash = results[0]
+
+                if hash_input == flag_hash:
+                    print("correct! well done.")
+                    self.log_attempt(True)
+                    self.update_progress()
+                    break
+                else:
+                    print("wrong answer, try again.")
+                    self.log_attempt(False)
+        except  mysql.connector.Error:
+               
+               print("Error verifying answer.")
+               sys.exit()  
+
+
+    def   log_attempt(self, success): # update the players log attempt, try to  track 
+        try: 
+            self.cursor.execute("insert into attempt_log(username , level , attempt_time, success) values (%s, %s, %s, %s)",(self.user_name,self.current_level, datetime.now(),success)) 
+
+            self.connection.commit()  
+
+        except mysql.connector.Error:
+            print("Error logging attempt.")
+            sys.exit()
+
+    def update_process(self):
+
+        try:
+            self.current_level +=1
+
+            self.cursor.execute(" update hunt_progress set current_level =%s where username =%s",(self.current_level,self.user_name)) 
+            self.connection.commit()
+            print(f"Advancing to the next level {self.user_name}.")
+        except mysql.connector.Error:
+            print("Error updating progress.")
+            sys.exit()
+
+    def TCP_Game(self):
+
+        try:
+
+            completion_time =datetime.mow()
+
+            self.cursor.execute("select username from leaderboard where username =%s",(self.user_name,))
+            results = self.cursor.fetchone()
+
+            if results is None:
+                self.cursor.execute("insert into leaderboard(username, completion_time) value(%s, %s)", (self.user_name, completion_time))
+                self.conncetion.commit()
+
+                print("congratulations!, You completed the IP hunt!.")
+                print(f"Completion time: {completion_time}")
+
+        except  mysql.connector.Error:
+            print("Error completing Game.")  
+            sys.exit()
+
+    def close_connection(self):
+        try:
+
+            if self.cursor:
+                self.cursor.close()
+            if self.connection:
+                self.connection.close()
+            print("Connection has closed.")
+
+        except mysql.connector.Error:
+            print("Error closing connection.")
+
+    def play_game(self):
+
+        self.get_user_input()
+        self.connect_database()
+        self.check_progress()
+
+        while self.current_level <=3:
+            self.get_challenge()
+            self.verify_answer()
+
+        if  self.current_level >3:
+            self.TCP_Game()
+
+        self.close_connection()          
+                                          
+
+if __name__ =="__main__":
+    game=GameClient()
+    game.play_game()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                             
+            
 
     
 
